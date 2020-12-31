bd72852314aaf051aa77d2dc1deb6e84ad6173b0
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-12-31 22:06:00 +0100

    add material app to app widget

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -5,9 +5,10 @@ import './book/books.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(title: Text('My Entertainment')),
       body: Books(),
-    );
+    ));
   }
 }
