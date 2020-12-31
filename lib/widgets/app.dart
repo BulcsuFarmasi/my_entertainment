580f3bf4e092f7a1794664f82b70fee250de2790
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-12-31 22:04:13 +0100

    create basic structure of books widgets

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
+import './book/books.dart';
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('My Entertainment'),));
+    return Scaffold(
+      appBar: AppBar(title: Text('My Entertainment')),
+      body: Books(),
+    );
   }
 }
