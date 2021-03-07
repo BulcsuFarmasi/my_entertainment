8f1e567cee194668879deefea270499c84fcb35a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-03-07 21:58:27 +0100

    add model for series

---
+++ lib/models/series.dart
@@ -0,0 +1,11 @@
+import 'package:meta/meta.dart';
+
+abstract class Series {
+  final String title;
+  // The date when the series aired its first episode
+  final DateTime premier;
+  // The date when the series aired its last episode
+  final DateTime finale;
+
+  Series({@required this.title, @required this.premier, this.finale});
+}
