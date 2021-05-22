25229f9e25f7f7850dd9e8d8258ba19a5e28410a
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-05-22 10:40:34 +0200

    add more films to state

--- lib/models/film_release.dart
+++ lib/models/film_release.dart
@@ -5,6 +5,7 @@ class FilmRelease extends Film {
   DateTime? localPremier;
   String? localTitle;
   final Language language;
+  bool original;
 
   FilmRelease(
       {required int id,
@@ -12,7 +13,7 @@ class FilmRelease extends Film {
       String? localTitle,
       required String title,
       DateTime? localPremier,
-      required this.language})
+      required this.language, this.original = false})
       : super(id: id, premier: premier, title: title) {
     this.localPremier ??= premier;
     this.localTitle ??= title;
--- lib/state/film/FilmState.dart
+++ lib/state/film/FilmState.dart
@@ -9,13 +9,35 @@ class FilmState {
         id: 1,
         premier: DateTime(2021, 3, 18),
         title: 'Zack Synder\'s Justice Leage',
-        language: Language(endonym: 'Engish (US)', code: 'en-US')),
+        language: Language(endonym: 'Engish (US)', code: 'en-US'), original: true),
     FilmRelease(
         id: 1,
         premier: DateTime(2021, 3, 18),
         localPremier: DateTime(2021, 3, 18),
         title: 'Zack Synder\'s Justice Leage',
         localTitle: 'Zack Synder: Az igazság ligája',
+        language: Language(endonym: 'magyar', code: 'hu-HU')),
+    FilmRelease(
+        id: 2,
+        premier: DateTime(2020, 11, 10),
+        title: 'Soul',
+        language: Language(endonym: 'Engish (US)', code: 'en-US'), original: true),
+    FilmRelease(
+        id: 2,
+        premier: DateTime(2020, 11, 10),
+        title: 'Soul',
+        localTitle: 'Lelki ismeretek',
+        language: Language(endonym: 'magyar', code: 'hu-HU')),
+    FilmRelease(
+        id: 3,
+        premier: DateTime(2022, 06, 30),
+        title: 'Minions: The Rise of Gru',
+        language: Language(endonym: 'Engish (US)', code: 'en-US'), original: true),
+    FilmRelease(
+        id: 3,
+        premier: DateTime(2022, 06, 30),
+        title: 'Minions: The Rise of Gru',
+        localTitle: 'Minyonok: Gru színre lép',
         language: Language(endonym: 'magyar', code: 'hu-HU')),
   ];
 
@@ -24,6 +46,16 @@ class FilmState {
       filmId: 1,
       releasesWatched: {films[0]: true, films[1]: true},
       filmWatchingState: FilmWatchingState.allReleasesWatched,
+    ),
+    FilmWatching(
+      filmId: 2,
+      releasesWatched: {films[2]: true, films[3]: false},
+      filmWatchingState: FilmWatchingState.partOfReleasesWatched,
+    ),
+    FilmWatching(
+      filmId: 3,
+      releasesWatched: {films[0]: false, films[1]: false},
+      filmWatchingState: FilmWatchingState.plannedToWatch,
     )
   ];
 }
