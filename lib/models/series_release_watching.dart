5dcb27a68c9943666956b4942bd1fefa3855aaab
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-06-21 21:27:18 +0200

    introduce remaining episodes property to series release wtaching

--- lib/models/series_release_watching.dart
+++ lib/models/series_release_watching.dart
@@ -5,6 +5,8 @@ class SeriesReleaseWatching {
   final SeriesRelease seriesRelease;
   final Episode currentEpisode;
   final bool isCompleted;
+  final int remainingEpisodes;
 
-  SeriesReleaseWatching({required this.seriesRelease, required this.currentEpisode, required this.isCompleted});
+
+  SeriesReleaseWatching({required this.seriesRelease, required this.currentEpisode, required this.isCompleted, required this.remainingEpisodes});
 }
