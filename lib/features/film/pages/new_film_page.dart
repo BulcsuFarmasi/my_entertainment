70e93d1de9a1ff9b2cfa2a18b111e683ab8e8855
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-07-09 22:39:52 +0200

    add new film page

---
+++ lib/features/film/pages/new_film_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NewFilmPage extends StatefulWidget {
+  @override
+  _NewFilmPageState createState() => _NewFilmPageState();
+}
+
+class _NewFilmPageState extends State<NewFilmPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
